9997fb68e4606e74f4cecec3749c1a158654448b
Vitor Borges <vitor27.sab@hotmail.com>
2023-01-24 15:27:03 -0300

    fetch getProducts is done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,35 @@
+"use client"; // this is a client component
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import { useEffect, useState } from 'react'
+
+import GetProducts from '../hooks/getProducts'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [productsData, setProductsData] = useState([]);
+
+  useEffect (() => {
+    const dataFetch = async () => {
+      try {
+        const parameters = {
+          page: 1,
+          row: 10,
+          sort: 'id' ,
+          order: 'ASC'
+      }
+
+        const data = await GetProducts(parameters);
+        setProductsData(data.products);
+      } catch (e){
+        console.log(e);
+      }
+    }
+
+    dataFetch();
+  }, []);
+  
 
   return (
     <main className={styles.main}>
@@ -12,6 +38,15 @@ export default function Home() {
               API GET PRODUCTS
             </h2>
 
+            <ul>
+          {productsData.map((product : any) => (
+            <li key={product.id}>
+              <h3>{product.name}</h3>
+              <span>{product.description}</span>
+              <span>{product.price}</span>
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   )
---
+++ src/hooks/getProducts.tsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+type parametersGet = {
+    page: number,
+    row: number,
+    sort: string,//sort?: 'id' | 'name' | 'price',
+    order: string//order?: 'DESC' | 'ASC'
+}
+
+const GetProducts = async ({page, row, sort, order} : parametersGet) => {
+    const url = `https://mks-challenge-api-frontend.herokuapp.com/api/v1/products?page=${page}&rows=${row}&sortBy=${sort}&orderBy=${order}`
+    
+    const res = await (await axios.get(url)).data;
+    return res;
+};
+
+export default GetProducts;
